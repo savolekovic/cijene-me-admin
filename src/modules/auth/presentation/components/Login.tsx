1a9c2d9340fc459de33e8a14a337f7036d67d387
Savo Lekovic <savo.lekovic@amplitudo.me>
2024-12-10 21:12:25 +0100

    test vercel

--- src/modules/auth/presentation/components/Login.tsx
+++ src/modules/auth/presentation/components/Login.tsx
@@ -62,7 +62,7 @@ function Login() {
               <input
                 type={showPassword ? "text" : "password"}
                 className="form-control"
-                placeholder="Tvoja lozinka"
+                placeholder="Tvoja lozinka TEST"
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
